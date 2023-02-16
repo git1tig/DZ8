2d7b0e470d41a3375bb0e1878ac3cfb7306f3fd7
ALexSer <5440918@gmail.com>
2023-02-17 00:39:07 +0300

    60 got changed to the better!

--- 60/Program.cs
+++ 60/Program.cs
@@ -6,13 +6,53 @@
 // 26(1,0,1) 55(1,1,1)
 
 
-int[,,] genRandArray(int firstDim, int secondDim, int thirdDim)
+int[,,] genRandArrayUniq(int firstDim, int secondDim, int thirdDim)
 {
     int[,,] array = new int[firstDim, secondDim, thirdDim];
+    int[] voc = new int[array.GetLength(0) * array.GetLength(1) * array.GetLength(2)];  // типа список того, что уже было
     Random randy = new Random();
+    bool nonUniq = false;  
+    int tempy = 0;
+    int vocIndex = 0; // индекс словаря текущий
+    int vocLength = 0; //длина словаря, что бы не перебирать лишнее
     for (int i = 0; i < firstDim; i++)
         for (int j = 0; j < secondDim; j++)
-            for (int k = 0; k < thirdDim; k++) array[i, j, k] = randy.Next(0, 9);
+            for (int k = 0; k < thirdDim; k++)
+            {
+                //Console.WriteLine($"Working on ({i},{j},{k})");
+                nonUniq = true;
+                while (nonUniq)
+                {
+                    tempy = randy.Next(1, 9);
+                    //Console.WriteLine($"tempy= {tempy}");
+                    if (vocLength == 0)
+                    {
+                        array[i, j, k] = tempy;
+                        voc[0] = tempy;
+                        vocLength++;
+                        vocIndex++;                      
+                        break;
+                    }
+                    nonUniq = false;
+                    for (int n = 0; n < vocLength; n++)
+                    {
+                        if (tempy == voc[n])
+                        {
+                            nonUniq = true;
+                            break;
+                        }
+                        //Console.Write($"tempy={tempy}, voc[{n}]={voc[n]}, tempy == voc[n] {tempy == voc[n]} nonunic={nonUniq} \n");
+                    }
+                    if (!nonUniq)
+                    {
+                        voc[vocIndex] = tempy;
+                        vocIndex++;
+                        vocLength++;
+                        array[i, j, k] = tempy;
+                    }
+                }
+            }
+
 
     return array;
 }
@@ -23,9 +63,9 @@ void printArrayMod(int[,,] array)
         for (int j = 0; j < array.GetLength(1); j++)
             for (int k = 0; k < array.GetLength(2); k++)
             {
-                Console.Write($" {array[i, j,k]}({i},{j},{k})");
+                Console.Write($" {array[i, j, k]}({i},{j},{k})");
             }
-             Console.WriteLine();
+        Console.WriteLine();
     }
 }
-printArrayMod(genRandArray(2,2,2));
+printArrayMod(genRandArrayUniq(2, 2, 2));
